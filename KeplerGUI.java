5b310a23fdc54a66f8d771339ff1b07fe5ca697c
Alex Schwartz <schwara5@my.erau.edu>
2019-02-13 17:12:53 -0500

    Initial UML Generated Java Classes. Use Notepad++ to open.

---
+++ KeplerGUI.java
@@ -0,0 +1,24 @@
+
+
+/**
+ * @author Owner
+ * @version 1.0
+ * @created 13-Feb-2019 5:09:32 PM
+ */
+public class KeplerGUI {
+
+	public KeplerGUI(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	public void Main(){
+
+	}
+
+	public void Start(){
+
+	}
+}//end KeplerGUI
---
+++ Manager.java
@@ -0,0 +1,26 @@
+
+
+/**
+ * @author Owner
+ * @version 1.0
+ * @created 13-Feb-2019 5:09:34 PM
+ */
+public class Manager {
+
+	public KeplerPane m_KeplerPane;
+
+	public Manager(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	public void calcGravitationalModel(){
+
+	}
+
+	public double getG(){
+		return 0;
+	}
+}//end Manager
