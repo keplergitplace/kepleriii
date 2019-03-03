7983f1c4281417df33e61a94488e00549f350ec5
Alex Schwartz <schwara5@my.erau.edu>
2019-03-03 14:02:21 -0500

    Updated GUI and Pane. Pane now draws dropdown menus.

--- KeplerGUI.java
+++ KeplerGUI.java
@@ -1,8 +1,6 @@
 import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.application.Application;
-import javafx.scene.control.Button;
 
 /**
  * @author Aaron Morgado
@@ -15,41 +13,22 @@ import javafx.scene.control.Button;
  */
 public class KeplerGUI extends Application{
 
-	private Pane mainPane = new Pane();
-	private Pane passedPane = new Pane();
+	KeplerPane keplerPane = new KeplerPane();
 	private Stage mainStage;
 	private Scene mmScene;
-	private boolean panePassed;
-
-	public KeplerGUI(){
-
-
-
-	}
+	
 
 	// main for testing window (might need to be changed to different main)
 	public static void main(String[] args) {
-
 		launch(args);
-
 	}
 
 	// window function
 	public void start(Stage mmStage) throws Exception {
-
-		Button btn = new Button("click me");
-		btn.setLayoutX(200);
-		btn.setLayoutY(200);
-
-		mainPane.getChildren().add(btn);
-
 		// Set the stage for main menu
 		mainStage = mmStage;
-
-
 		//give a pane to the scene
-		System.out.println("ya yeet\n");
-		mmScene = new Scene(passedPane, 600, 600);
+		mmScene = new Scene(keplerPane, 600, 600);
 		mainStage.setMaximized(true);
 		mainStage.setScene(mmScene);
 		mainStage.setTitle("Kepler III");
--- KeplerPane.java
+++ KeplerPane.java
@@ -1,50 +1,85 @@
-import javafx.application.Application;
-import javafx.scene.Scene;
+import java.util.ArrayList;
+
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
-/**
- * @author Owner
- * @version 1.0
- * @created 13-Feb-2019 5:09:33 PM
- */
-public class KeplerPane {
+	/**
+	 * @author Joint Project
+	 * @version 1.0
+	 * @created 13-Feb-2019 5:09:33 PM
+	 */
+	public class KeplerPane extends Pane {
 
-	private Button Earth;
-	private Button Exit;
-	private Button Help;
-	private Button Import;
-	private Button Options;
-	private MenuList Planets;
-	private Button SandboxMode;
-	private MenuList Stars;
+		private Button Earth;
+		private Button Exit;
+		private Button Help;
+		private Button Import;
+		private Button Options;
+		private MenuItem[] planet = new MenuItem[9];
+		MenuItem star = new MenuItem("Sun");
+		MenuBar menuBar = new MenuBar();
+		Menu Planets = new Menu("Planets");
+		Menu Stars = new Menu("Stars");
+		private String planetName[] = {"Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"};
+		private Button SandboxMode;
 
-	public KeplerPane(){
-		genericPane = new Pane();
-		Earth = new Button("Earth");
-		Exit = new Button("Exit");
-		Help = new Button("Help");
-		Import = new Button("Import");
-		Options = new Button("Options");
+		public KeplerPane(){
+			start();
+		}
 		
-		Import.setLayoutX(250);
-		Import.setLayoutY(120);
+		public void start(){
+			Earth = new Button("Earth");
+			Exit = new Button("Exit");
+			Help = new Button("Help");
+			Import = new Button("Import");
+			Options = new Button("Options");
+			
+			Import.setLayoutX(250);
+			Import.setLayoutY(120);
+			
+			Earth.setLayoutX(250);
+			Earth.setLayoutY(160);
+			
+			Help.setLayoutX(250);
+			Help.setLayoutY(200);
+			
+			Options.setLayoutX(250);
+			Options.setLayoutY(240);
+			
+			Exit.setLayoutX(250);
+			Exit.setLayoutY(280);
+			
+			getChildren().addAll(Earth, Exit, Help, Import, Options);
+			Earth.setOnAction( e-> {
+				Default();
+			});
+		}
 		
-		Earth.setLayoutX(250);
-		Earth.setLayoutY(160);
+	public void Default() {
+		getChildren().clear();
+		start();
+		earthLists();
+		}
+	
+	public void earthLists() {
+	//	Planets = new MenuBar();
+		menuBar.getMenus().addAll(Planets, Stars);
+		getChildren().add(menuBar);
+		menuBar.setLayoutY(10);
+		menuBar.setLayoutX(400);
+			for(int i =0; i < planetName.length; i++) {
+				planet[i] = new MenuItem(planetName[i]);
+				Planets.getItems().add(planet[i]);
+			}
+			Stars.getItems().add(star);
+			
+			planet[2].setOnAction( e-> {
+			
+			});
 		
-		Help.setLayoutX(250);
-		Help.setLayoutY(200);
-		
-		Options.setLayoutX(250);
-		Options.setLayoutY(240);
-		
-		Exit.setLayoutX(250);
-		Exit.setLayoutY(280);
-		
-		genericPane.getChildren().addAll(Earth, Exit, Help, Import, Options);
-	}
-	}
+		}
 
 }//end KeplerPane
