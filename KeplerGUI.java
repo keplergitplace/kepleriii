634cda91def86bed84dd4076ca839074053a9ac7
Alex Schwartz <schwara5@my.erau.edu>
2019-04-03 15:02:01 -0400

    Music functionality. No loner need music.java file. Aaron did this.

--- KeplerGUI.java
+++ KeplerGUI.java
@@ -1,7 +1,9 @@
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.application.Application;
-
+import java.io.File;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 /**
  * @author Aaron Morgado
  * @version 0.1
@@ -16,13 +18,22 @@ public class KeplerGUI extends Application{
 	KeplerPane keplerPane = new KeplerPane();
 	private Stage mainStage;
 	private Scene mmScene;
-	
-	
+
+
 	/** The window function
 	 * @param mmStage, keplerPane
 	 * @see KeplerPane
 	 */
 	public void start(Stage mmStage) throws Exception {
+		// set up music player
+		//C:\Users\Owner\Desktop\Creedence Clearwater Revival - Fortunate Son [Music Video].mp3 fortunate son
+		//C:\Users\Owner\Desktop\Interstellar Main Theme - Extra Extended - Soundtrack by Hans Zimmer.mp3 Interstellar
+		String path = "C:\\Users\\Owner\\Desktop\\Interstellar Main Theme - Extra Extended - Soundtrack by Hans Zimmer.mp3";
+		Media music = new Media(new File(path).toURI().toString());
+		MediaPlayer song = new MediaPlayer(music);
+		song.setVolume(10);
+		song.setAutoPlay(true);
+
 		// Set the stage for main menu
 		mainStage = mmStage;
 		//give a pane to the scene
@@ -32,13 +43,13 @@ public class KeplerGUI extends Application{
 		mainStage.setTitle("Kepler III");
 		//display GUI
 		mainStage.show();
-		
+
+	}
 	/** Main for testing window
 	 * 
 	 */
 	public static void main(String[] args) {
-			launch(args);
-		}
+		launch(args);
 
 	}
 
