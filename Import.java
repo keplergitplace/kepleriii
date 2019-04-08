fe091e91d62e8b1c84d9529094bf02e1ef593581
Alex Schwartz <schwara5@my.erau.edu>
2019-04-08 13:42:57 -0400

    Changed name of main function so it can be called from other classes

--- Import.java
+++ Import.java
@@ -31,7 +31,7 @@ public class Import
 	public static ArrayList<String> sRadius = new ArrayList<String>();
 	public static ArrayList<String> sMass = new ArrayList<String>();
 	public static ArrayList<String> sAge = new ArrayList<String>();
-	
+
 	public static ArrayList<Double> orbitPeriodNum = new ArrayList<Double>();
 	public static ArrayList<Double> semiMajorNum = new ArrayList<Double>();
 	public static ArrayList<Double> eccentricityNum = new ArrayList<Double>();
@@ -44,14 +44,14 @@ public class Import
 	public static ArrayList<Double> sRadiusNum = new ArrayList<Double>();
 	public static ArrayList<Double> sMassNum = new ArrayList<Double>();
 	public static ArrayList<Double> sAgeNum = new ArrayList<Double>();
-	
+
 	public static void convertToOrbitDouble()
 	{
 		//System.out.println("Test"); //Used for Debugging
 		orbitPeriodNum.add(null); //First index is non-essential
 		for(int i = 1; i < orbitPeriod.size(); i++)
 		{
-		    //orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
+			//orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
 			//orbitPeriodNum.add(Double.parseDouble(orbitPeriod.get(i)));
 			if(orbitPeriod.get(i).length()!=0)
 			{
@@ -63,14 +63,14 @@ public class Import
 			}
 		}
 	}
-	
+
 	public static void convertToEccentricityDouble()
 	{
 		//System.out.println("Test"); //Used for Debugging
 		eccentricityNum.add(null); //First index is non-essential
 		for(int i = 1; i < semiMajor.size(); i++)
 		{
-		    //orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
+			//orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
 			//orbitPeriodNum.add(Double.parseDouble(orbitPeriod.get(i)));
 			if(eccentricity.get(i).length()!=0)
 			{
@@ -82,14 +82,14 @@ public class Import
 			}
 		}
 	}
-	
+
 	public static void convertToPlanetRadDouble()
 	{
 		//System.out.println("Test"); //Used for Debugging
 		planetRadNum.add(null); //First index is non-essential
 		for(int i = 1; i < planetRad.size(); i++)
 		{
-		    //orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
+			//orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
 			//orbitPeriodNum.add(Double.parseDouble(orbitPeriod.get(i)));
 			if(planetRad.get(i).length()!=0)
 			{
@@ -101,14 +101,14 @@ public class Import
 			}
 		}
 	}
-	
+
 	public static void convertToPlanetDistanceDouble()
 	{
 		//System.out.println("Test"); //Used for Debugging
 		planetDistanceNum.add(null); //First index is non-essential
 		for(int i = 1; i < planetDistance.size(); i++)
 		{
-		    //orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
+			//orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
 			//orbitPeriodNum.add(Double.parseDouble(orbitPeriod.get(i)));
 			if(planetDistance.get(i).length()!=0)
 			{
@@ -120,14 +120,14 @@ public class Import
 			}
 		}
 	}
-	
+
 	public static void convertToPlanetDensityDouble()
 	{
 		//System.out.println("Test"); //Used for Debugging
 		planetDensityNum.add(null); //First index is non-essential
 		for(int i = 1; i < planetDensity.size(); i++)
 		{
-		    //orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
+			//orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
 			//orbitPeriodNum.add(Double.parseDouble(orbitPeriod.get(i)));
 			if(planetDensity.get(i).length()!=0)
 			{
@@ -139,14 +139,14 @@ public class Import
 			}
 		}
 	}
-	
+
 	public static void convertToPlanetMassDouble()
 	{
 		//System.out.println("Test"); //Used for Debugging
 		planetMassNum.add(null); //First index is non-essential
 		for(int i = 1; i < semiMajor.size(); i++)
 		{
-		    //orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
+			//orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
 			//orbitPeriodNum.add(Double.parseDouble(orbitPeriod.get(i)));
 			if(planetMass.get(i).length()!=0)
 			{
@@ -158,14 +158,14 @@ public class Import
 			}
 		}
 	}
-	
+
 	public static void convertToSemiDouble()
 	{
 		//System.out.println("Test"); //Used for Debugging
 		semiMajorNum.add(null); //First index is non-essential
 		for(int i = 1; i < semiMajor.size(); i++)
 		{
-		    //orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
+			//orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
 			//orbitPeriodNum.add(Double.parseDouble(orbitPeriod.get(i)));
 			if(semiMajor.get(i).length()!=0)
 			{
@@ -177,14 +177,14 @@ public class Import
 			}
 		}
 	}
-	
+
 	public static void convertToSsGravityDouble()
 	{
 		//System.out.println("Test"); //Used for Debugging
 		ssGravityNum.add(null); //First index is non-essential
 		for(int i = 1; i < ssGravity.size(); i++)
 		{
-		    //orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
+			//orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
 			//orbitPeriodNum.add(Double.parseDouble(orbitPeriod.get(i)));
 			if(ssGravity.get(i).length()!=0)
 			{
@@ -196,14 +196,14 @@ public class Import
 			}
 		}
 	}
-	
+
 	public static void convertToSLuminDouble()
 	{
 		//System.out.println("Test"); //Used for Debugging
 		sLuminNum.add(null); //First index is non-essential
 		for(int i = 1; i < sLumin.size(); i++)
 		{
-		    //orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
+			//orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
 			//orbitPeriodNum.add(Double.parseDouble(orbitPeriod.get(i)));
 			if(sLumin.get(i).length()!=0)
 			{
@@ -215,14 +215,14 @@ public class Import
 			}
 		}
 	}
-	
+
 	public static void convertToSRadiusDouble()
 	{
 		//System.out.println("Test"); //Used for Debugging
 		sRadiusNum.add(null); //First index is non-essential
 		for(int i = 1; i < ssGravity.size(); i++)
 		{
-		    //orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
+			//orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
 			//orbitPeriodNum.add(Double.parseDouble(orbitPeriod.get(i)));
 			if(sRadius.get(i).length()!=0)
 			{
@@ -234,14 +234,14 @@ public class Import
 			}
 		}
 	}
-	
+
 	public static void convertToSMassDouble()
 	{
 		//System.out.println("Test"); //Used for Debugging
 		sMassNum.add(null); //First index is non-essential
 		for(int i = 1; i < sMass.size(); i++)
 		{
-		    //orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
+			//orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
 			//orbitPeriodNum.add(Double.parseDouble(orbitPeriod.get(i)));
 			if(sRadius.get(i).length()!=0)
 			{
@@ -253,14 +253,14 @@ public class Import
 			}
 		}
 	}
-	
+
 	public static void convertToSAgeDouble()
 	{
 		//System.out.println("Test"); //Used for Debugging
 		sAgeNum.add(null); //First index is non-essential
 		for(int i = 1; i < sAge.size(); i++)
 		{
-		    //orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
+			//orbitPeriodNum[i] = Double.parseDouble(orbitPeriod[i]);
 			//orbitPeriodNum.add(Double.parseDouble(orbitPeriod.get(i)));
 			if(sAge.get(i).length()!=0)
 			{
@@ -289,7 +289,7 @@ public class Import
 		reader.close();
 		//System.out.println(planetData.get(110)); //Planet Data does not start till line 110
 	}
-	
+
 	/**
 	 * Return the Arraylist of Planet Data
 	 * @param void
@@ -299,7 +299,7 @@ public class Import
 	{
 		return planetData;
 	}
-	
+
 	/*public static void parseData() //Failed Experiment
 	{
 		String line = "";
@@ -309,44 +309,44 @@ public class Import
 			//hostName.addAll(line.split(",",0));
 			System.out.println(line.split(",",0));
 			System.out.println(line);
-			
+
 		}
 	}*/
-	
+
 	/**
 	 * Stores default values for exoplanets.csv
 	 * @param void
 	 */
 	public static void storeDataDefault() throws Exception
 	{
-			//Import.planetData.clear();
-			BufferedReader reader = new BufferedReader(new FileReader("exoplanets.csv"));
-			String line;
-			while ((line = reader.readLine()) != null)
-			{
-				String[] splitLin = line.split(",");
-				hostName.add(splitLin[0]);
-				planetLetter.add(splitLin[1]);
-				planetName.add(splitLin[2]);
-				discoveryMethod.add(splitLin[3]);
-				cFlag.add(splitLin[4]);
-				yearDisc.add(splitLin[5]);
-				orbitPeriod.add(splitLin[6]);//Need double
-				semiMajor.add(splitLin[11]);//Need Double
-				eccentricity.add(splitLin[16]);//Need Double
-				planetMass.add(splitLin[21]);//Need double
-				planetRad.add(splitLin[27]);//Need Double
-				planetDensity.add(splitLin[32]);//Need Double
-				planetDistance.add(splitLin[52]);//Need Double
-				//ssGravity.add(splitLin[74]); //Errors will occur due to no data. 
-				//sLumin.add(splitLin[79]);
-				//sRadius.add(splitLin[89]);
-				//sMass.add(splitLin[84]);
-				//sAge.add(splitLin[100]);
-			}
-			reader.close();
+		//Import.planetData.clear();
+		BufferedReader reader = new BufferedReader(new FileReader("exoplanets.csv"));
+		String line;
+		while ((line = reader.readLine()) != null)
+		{
+			String[] splitLin = line.split(",");
+			hostName.add(splitLin[0]);
+			planetLetter.add(splitLin[1]);
+			planetName.add(splitLin[2]);
+			discoveryMethod.add(splitLin[3]);
+			cFlag.add(splitLin[4]);
+			yearDisc.add(splitLin[5]);
+			orbitPeriod.add(splitLin[6]);//Need double
+			semiMajor.add(splitLin[11]);//Need Double
+			eccentricity.add(splitLin[16]);//Need Double
+			planetMass.add(splitLin[21]);//Need double
+			planetRad.add(splitLin[27]);//Need Double
+			planetDensity.add(splitLin[32]);//Need Double
+			planetDistance.add(splitLin[52]);//Need Double
+			//ssGravity.add(splitLin[74]); //Errors will occur due to no data. 
+			//sLumin.add(splitLin[79]);
+			//sRadius.add(splitLin[89]);
+			//sMass.add(splitLin[84]);
+			//sAge.add(splitLin[100]);
+		}
+		reader.close();
 	}
-	
+
 	/**
 	 * Stores default values from the parameter name
 	 * @param String Name
@@ -354,34 +354,34 @@ public class Import
 	 */
 	public static void storeData(String name) throws Exception
 	{
-			//Import.planetData.clear();
-			BufferedReader reader = new BufferedReader(new FileReader(name));
-			String line;
-			while ((line = reader.readLine()) != null)
-			{
-				String[] splitLin = line.split(",");
-				hostName.add(splitLin[0]);
-				planetLetter.add(splitLin[1]);
-				planetName.add(splitLin[2]);
-				discoveryMethod.add(splitLin[3]);
-				cFlag.add(splitLin[4]);
-				yearDisc.add(splitLin[5]);
-				orbitPeriod.add(splitLin[6]);//Need Double
-				semiMajor.add(splitLin[11]);//Need Double
-				eccentricity.add(splitLin[16]);//Need Double
-				planetMass.add(splitLin[21]);//Need Double
-				planetRad.add(splitLin[27]);//Need Double
-				planetDensity.add(splitLin[32]);//Need Double
-				planetDistance.add(splitLin[52]);//Need Double
-				//ssGravity.add(splitLin[74]); //Errors will occur due to no data. 
-				//sLumin.add(splitLin[79]);
-				//sRadius.add(splitLin[89]);
-				//sMass.add(splitLin[84]);
-				//sAge.add(splitLin[100]);
-			}
-			reader.close();
+		//Import.planetData.clear();
+		BufferedReader reader = new BufferedReader(new FileReader(name));
+		String line;
+		while ((line = reader.readLine()) != null)
+		{
+			String[] splitLin = line.split(",");
+			hostName.add(splitLin[0]);
+			planetLetter.add(splitLin[1]);
+			planetName.add(splitLin[2]);
+			discoveryMethod.add(splitLin[3]);
+			cFlag.add(splitLin[4]);
+			yearDisc.add(splitLin[5]);
+			orbitPeriod.add(splitLin[6]);//Need Double
+			semiMajor.add(splitLin[11]);//Need Double
+			eccentricity.add(splitLin[16]);//Need Double
+			planetMass.add(splitLin[21]);//Need Double
+			planetRad.add(splitLin[27]);//Need Double
+			planetDensity.add(splitLin[32]);//Need Double
+			planetDistance.add(splitLin[52]);//Need Double
+			//ssGravity.add(splitLin[74]); //Errors will occur due to no data. 
+			//sLumin.add(splitLin[79]);
+			//sRadius.add(splitLin[89]);
+			//sMass.add(splitLin[84]);
+			//sAge.add(splitLin[100]);
+		}
+		reader.close();
 	}
-	
+
 	/**
 	 * This method has the user select the file they wish to view.
 	 * @param void
@@ -398,21 +398,21 @@ public class Import
 		frame.dispose();
 		return filename;
 	}
-	
+
 	/** The "main" method of the Import function, call this to run everything else.
 	 * @param void
 	 */
 	//public static void run()
-	public static void main(String[] args)
+	public void run()
 	{
 		//Calls the method Name and saves the returned file path to a string.
 		String filePath = Name();
 		//I/O Exception handling; if the user did not select anything and closed the window it is handled in the if statement.
 		if (filePath == null)
-				System.out.println("You cancelled the choice");
+			System.out.println("You cancelled the choice");
 		else
-				System.out.println("You chose "+filePath);
-		
+			System.out.println("You chose "+filePath);
+
 		try {//Reading File
 			storeData(filePath);
 		}catch(Exception e1) {
@@ -422,7 +422,7 @@ public class Import
 		{
 			System.out.println(line);
 		}*/
-		
+
 		convertToOrbitDouble();
 		convertToEccentricityDouble();
 		convertToPlanetRadDouble();
@@ -439,7 +439,7 @@ public class Import
 		{
 			System.out.println(planetRadNum.get(j));
 		}
-		
+
 		//This is for testing of the default method. Not used for functionality
 		/*try {
 			storeDataDefault();
@@ -453,6 +453,6 @@ public class Import
 			  							//line 110. CSV file may need to be
 			  							//Modified
 		}*/
-		
+
 	}
 }
