98f6ce83abdf6b0e87b7bdf55313103e34303ed2
VivaciousViolist <blackc12@my.erau.edu>
2019-04-23 22:08:12 -0400

    no message

--- Export.java
+++ Export.java
@@ -1,4 +1,13 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Writer;
 
 /**
  * @author Owner
@@ -9,6 +18,7 @@ public class Export {
 
 	private File String;
 	public Manager m_Manager;
+	private SandBox sBox = new SandBox();
 
 	public Export(){
 
@@ -21,7 +31,20 @@ public class Export {
 		return "";
 	}
 
-	public void printData(){
-
+	public void printData()
+	{
+		PrintWriter out = null;
+		try
+		{
+			out = new PrintWriter(new BufferedWriter(new FileWriter("SandBox.csv")));
+			out.println("This is a test to see if the print statement work");
+		}catch(IOException e2)
+		{
+			System.err.println(e2);
+		}finally {
+			if(out != null)
+				out.close();
+		}
 	}
+	
 }//end Export
